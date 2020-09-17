d3bd8bbf4b8f413ac55c8c5254a06093c27f3be3
lvtraveler <xiezhihai7@163.com>
2020-09-18 00:30:40 +0800

    first commit

---
+++ src/app/report/report.component.ts
@@ -0,0 +1,115 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpDomainService } from "../services/http-domain.service";
+// import { timingSafeEqual } from 'crypto';
+
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.component.html',
+  styleUrls: ['./report.component.css']
+})
+export class ReportComponent implements OnInit {
+
+  public home;
+  public option;
+  expenseDetail: any;
+  public expenseRepostList: Object[] = [];
+  constructor(public httpDomain: HttpDomainService) { }
+
+  ngOnInit(): void {
+    var api_home = "/home"
+    this.httpDomain.get(api_home).then((response) => {
+      this.home = response;
+    })
+    var api_expense = "expenses/details";
+    var xData: any[] = [];
+    var yData: any[] = [0];
+    var maxDate = 0;
+    this.httpDomain.get(api_expense).then((response) => {
+      this.expenseDetail = response;
+      // console.log(this.expenseDetail)
+      for(let i=0;i<this.expenseDetail.length;i++) {
+        for(let j=0;j<this.expenseDetail[i].expenseItems.length;j++){
+          this.expenseRepostList.push(this.expenseDetail[i].expenseItems[j]);
+        }
+      }
+      for (var i = 0; i < this.expenseDetail.length; i++) {
+        xData.push(this.expenseDetail[i]["categoryName"]);
+        yData.push(this.expenseDetail[i]["totalValue"]);
+        if (this.expenseDetail[i]['totalValue'] > maxDate) maxDate = this.expenseDetail[i]['totalValue']
+      }
+    })
+    var dataAxis = xData;
+    var data = yData;
+    var yMax = maxDate;
+    var dataShadow = [];
+
+    for (var i = 0; i < data.length; i++) {
+      dataShadow.push(yMax);
+    }
+    this.option = {
+      title: {
+        text: 'Expenses',
+        subtext: 'Personal Finance Manager: Expenses Manager'
+      },
+      xAxis: {
+        data: dataAxis,
+        axisLabel: {
+          inside: true,
+          textStyle: {
+            color: '#fff'
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        axisLine: {
+          show: false
+        },
+        z: 10
+      },
+      yAxis: {
+        axisLine: {
+          show: false
+        },
+        axisTick: {
+          show: false
+        },
+        axisLabel: {
+          textStyle: {
+            color: '#999'
+          }
+        }
+      },
+      dataZoom: [
+        {
+          type: 'inside'
+        }
+      ],
+      series: [
+        { // For shadow
+          type: 'bar',
+          itemStyle: {
+            color: 'rgba(0,100,0,0.05)'
+          },
+          barGap: '-100%',
+          barCategoryGap: '40%',
+          data: dataShadow,
+          animation: false
+        },
+        {
+          type: 'bar',
+          itemStyle: {
+            color: { offset: 1, color: '#18Cdf0' }
+          },
+          emphasis: {
+            itemStyle: {
+              color: { offset: 0, color: '#2378f7' },
+            }
+          },
+          data: data
+        }
+      ]
+    };
+
+  }
+}
---
+++ src/app/services/http-domain.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpDomainService {
+  public domain = "http://106.15.238.105:8080/";
+  constructor(public http: HttpClient) { }
+  get(api) {
+    return new Promise((resolve, reject) => {
+      this.http.get(this.domain + api).subscribe(response => {
+        resolve(response);
+      }, (error) => {
+        reject(error);
+      })
+    })
+  }
+  put(api, json) {
+    return new Promise((resolve, reject) => {
+      this.http.put(this.domain + api, json).subscribe(response => {
+        resolve(response);
+      }, (error) => {
+        reject(error);
+      })
+    })
+  }
+  post(api, json) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.domain + api, json).subscribe(response => {
+        resolve(response);
+      }, (error) => {
+        reject(error);
+      })
+    })
+  }
+  delete(api, json) {
+    return new Promise((resolve, reject) => {
+      this.http.delete(this.domain + api, json).subscribe(response => {
+        resolve(response);
+      }, (error) => {
+        reject(error);
+      })
+    })
+
+  }
+}
